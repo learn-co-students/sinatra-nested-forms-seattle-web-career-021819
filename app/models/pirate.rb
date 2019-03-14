e226fd02db13ae7346f245701dea8baa88665e63
github username <github email address>
2019-03-14 08:28:02 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_reader :name, :weight, :height
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+  attr_reader :name, :type, :booty
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+
 end
